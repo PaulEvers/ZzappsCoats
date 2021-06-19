ab6f985b11ceb21fed271388e6eb14dfd0d7e6a0
Paul <paulevers1311@gmail.com>
2021-06-19 15:23:16 +0200

    Home hero

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { HeroComponent } from './hero/hero.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HeroComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
