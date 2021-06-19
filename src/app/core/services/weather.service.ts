05ec61b3ebbceb0a25ed05b556b433c7a27d635b
Paul <paulevers1311@gmail.com>
2021-06-19 13:10:13 +0200

    Set required properties depending on weather

--- src/app/core/services/weather.service.ts
+++ src/app/core/services/weather.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ComponentProperties } from '../classes/ComponentProperties';
 import { WeatherData } from '../classes/WeatherData';
 
 @Injectable({
@@ -15,6 +16,16 @@ export class WeatherService {
   }
 
   getCoatRecommendation(weatherData: WeatherData) {
+    const requiredProperties: ComponentProperties[] = [];
 
+    weatherData.isRaining ? requiredProperties.push(ComponentProperties.RAIN_RESISTENT) : '';
+    weatherData.isWindy ? requiredProperties.push(ComponentProperties.WIND_RESISTENT) : '';
+    weatherData.temperature < 0 ? requiredProperties.push(ComponentProperties.FREEZE_RESISTENT) : '';
+    weatherData.temperature >= 0 && weatherData.temperature < 12 ?  requiredProperties.push(ComponentProperties.COLD_COMFORTABLE) : ''; // TODO: Er is gekozen voor 12 graden als koud genoeg om extra comfort toe te voegen. Is deze temperatuur geschikt daarvoor?
+
+  }
+
+  private getComponentsFromProperties(properties: ComponentProperties[]) {
+    
   }
 }
