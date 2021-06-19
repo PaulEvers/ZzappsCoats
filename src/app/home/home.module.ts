a3c4645107c8ef3f8ef41814b7fec10d5b892307
Paul <paulevers1311@gmail.com>
2021-06-19 16:40:48 +0200

    Recommendation

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { HeroComponent } from './hero/hero.component';
+import { RecommendationComponent } from './recommendation/recommendation.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [HomeComponent, HeroComponent],
+  declarations: [HomeComponent, HeroComponent, RecommendationComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
